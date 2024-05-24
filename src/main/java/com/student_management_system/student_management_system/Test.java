7bb94e633de755a3da94960e837271b1131dfc4f
AdiDayat12 <adilinan72@gmail.com>
2024-05-24 11:00:28 +0300

    test

---
+++ src/main/java/com/student_management_system/student_management_system/Test.java
@@ -0,0 +1,7 @@
+package com.student_management_system.student_management_system;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
